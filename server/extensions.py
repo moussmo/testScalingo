13240e8526f2acec3a0b5e0c004b97454dcfd254
momomo <mouss.mo.ke@gmail.com>
2022-01-21 14:06:25 +0100

    Boutons Utilisateurs et sidebar enlevÃ©s

--- server/extensions.py
+++ server/extensions.py
@@ -1,8 +1,9 @@
 from extension_model import Extension
 
-extension1 = Extension('myExtension').addCalendar(name='calendar')\
-    .addInternship(name='Stages')\
+extension1 = Extension('myExtension').addCalendar(name='calendar').addInternship(name='Stages')\
     .addInternship(name='Fiche de stage vide', content='new')
 extension2 = Extension('myExtension2').addCalendar(name='calendar').addInternship(name='Stages')
+extension3 = Extension('myExtension3').addCalendar(name='calendar').addInternship(name='Stages')
+extension4 = Extension('myExtension4').addCalendar(name='calendar').addInternship(name='Stages')
 
-extensions_list = [extension1, extension2]
+extensions_list = [extension1, extension2, extension3, extension4]
